685c1a87115a5cccc5bb935854c480e969b5ea29
Cfairbanx <69484576+Cfairbanx@users.noreply.github.com>
2020-12-03 17:15:06 -0800

    v1
    Program to retrieve and store data types

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.ReadLine();
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("What page number are you on?");
+            string pageNumber = Console.ReadLine();
+            int num = Convert.ToInt32(pageNumber);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string needHelp = Console.ReadLine();
+            bool answer = Convert.ToBoolean(needHelp);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string yourExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string yourFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudy = Console.ReadLine();
+            int study = Convert.ToInt32(hoursStudy);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
+
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString();
+        }
+    }
+}
